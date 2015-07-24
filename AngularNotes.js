19119dc2df09bcc848d92f386c9800098d33e488
Greg Domorski <greg.domorski@gmail.com>
2015-07-23 21:07:09 -0700

    Update AngularNotes.js

--- AngularNotes.js
+++ AngularNotes.js
@@ -10,6 +10,13 @@ The ng-model directive binds the value of HTML controls (input, select, textarea
 
 The ng-bind directive binds application data to the HTML view.
 
+AngularJS modules define AngularJS applications.
+
+AngularJS controllers control AngularJS applications.
+
+The ng-app directive defines the application, the ng-controller directive defines the controller.
+
+
 */
 <html ng-app="nameApp">
 Lists (ng-repeat)
